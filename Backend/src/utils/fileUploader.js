b7122d61523b01389172da4db9346c94cdd01e99
chondon_kumar <chondon914@gmail.com>
2025-09-05 02:44:04 +0600

    feat: add file uploader utility for Cloudinary integration

---
+++ Backend/src/utils/fileUploader.js
@@ -0,0 +1,36 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+});
+
+const fileUploader = async (localFilePath) => {
+    try {
+    if (!localFilePath) { 
+        console.log("this local file path could not found")
+        return null
+    }
+
+    const response = await cloudinary.uploader.upload(localFilePath, 
+        {resource_type : 'auto'}
+    )
+    // file upload on cloudinary
+     console.log("file upload on cloudinary successfully", response.url);
+
+     return response.url
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove file from local uploads folder
+        console.log("error while uploading file on cloudinary", error.message);
+        return null
+    }
+}
+
+export {fileUploader}
